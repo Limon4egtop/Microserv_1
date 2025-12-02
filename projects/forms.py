f8c6a08735c7c6016e2c65c80f156e5b3446ac2e
Limon4egtop (t.me/Limon4egtop) <vowa.fvn2005@yandex.ru>
2025-12-02 17:18:17 +0300

    выполнен рефакторинг

--- projects/forms.py
+++ projects/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from .models import Project, Stage
+
+
 class ProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ["name","address","description"]
+        fields = ["name", "address", "description"]
+
+
 class StageForm(forms.ModelForm):
     class Meta:
         model = Stage
-        fields = ["project","name","start_date","end_date"]
+        fields = ["project", "name", "start_date", "end_date"]
--- projects/views.py
+++ projects/views.py
@@ -3,15 +3,21 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView
 from .models import Project
 from .forms import ProjectForm
+
+
 class ProjectListView(LoginRequiredMixin, ListView):
     model = Project
     template_name = "projects/project_list.html"
     context_object_name = "projects"
+
+
 class ProjectCreateView(LoginRequiredMixin, CreateView):
     model = Project
     form_class = ProjectForm
     template_name = "projects/project_form.html"
     success_url = reverse_lazy("projects:list")
+
+
 class ProjectUpdateView(LoginRequiredMixin, UpdateView):
     model = Project
     form_class = ProjectForm
